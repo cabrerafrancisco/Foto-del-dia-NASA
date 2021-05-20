3d3e13a6a63398aa52dae681cc9b83de376684a1
franciCabrera <francabre97@gmail.com>
2021-05-20 19:32:44 -0300

    agregue los primeros cambios

---
+++ ej1/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { request } = require('http');
+const path = require("path");
+const app = express()
+const PORT = 3001;
+
+
+app.use(express.static(path.join(__dirname, "client")));
+/*linkeados en html como css o js o imagen ... */
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "client/index.html"));
+})
+
+app.listen(PORT , function(){
+    console.log(`server iniciado en el puerto ${PORT}...`)
+})
